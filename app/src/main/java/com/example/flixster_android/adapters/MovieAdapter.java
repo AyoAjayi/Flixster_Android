cc379f5cb68baaff07b660150f3dfdb6e612bc0e
Ayo <ayomide.ajayi2839@gmail.com>
2020-06-16 20:25:27 -0400

    Completed Flixster 2 Assignment

--- app/src/main/java/com/example/flixster_android/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flixster_android/adapters/MovieAdapter.java
@@ -1,20 +1,26 @@
 package com.example.flixster_android.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.flixster_android.DetailActivity;
 import com.example.flixster_android.R;
 import com.example.flixster_android.models.Movie;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -60,6 +66,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
 
     // The view holder is a representation of our row in the recycler view
     public class ViewHolder extends RecyclerView.ViewHolder{
+        RelativeLayout container;
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
@@ -70,24 +77,41 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
         }
 
-        public void bind(Movie movie) {
+        public void bind(final Movie movie) {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
             String imageUrl;
             // if phone is in landscape mode
-            if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            if (context.getResources()
+
+                    .getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
                 // then imageUrl to be the backdrop image
                 imageUrl = movie.getBackdropPath();
             } else {
                 // else imageUrl is the poster image
                 imageUrl = movie.getPosterPath();
             }
-
-
-
             Glide.with(context).load(imageUrl).into(ivPoster);
+
+            // Register click listener on the whole row
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Navigate to a new activity on tap
+                    Intent i = new Intent(context, DetailActivity.class);
+                    // Passing in the movie object to other activity
+                    i.putExtra("movie", Parcels.wrap(movie));
+
+
+                    context.startActivity(i);
+
+
+
+                }
+            });
 
         }
     }
--- app/src/main/java/com/example/flixster_android/models/Movie.java
+++ app/src/main/java/com/example/flixster_android/models/Movie.java
@@ -3,16 +3,23 @@ package com.example.flixster_android.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
 
     String posterPath;
     String title;
     String overview;
     String backdropPath;
+    int movieId;
+    double rating;
+
+    // empty constructor needed by the Parceler Library
+    public Movie(){}
 
     // Create a constructor that takes in JSON Objects
 
@@ -21,6 +28,8 @@ public class Movie {
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
 
     }
 
@@ -50,5 +59,13 @@ public class Movie {
 
     public String getBackdropPath() {
         return String.format("https://image.tmdb.org/t/p/w342/%s", backdropPath);
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public int getMovieId() {
+        return movieId;
     }
 }
